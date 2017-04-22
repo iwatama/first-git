e4aed526e94a2a1bec629832f3b6bb4bddae1edc
iwata <iwaking43@gmail.com>
2017-04-22 02:35:38 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170416051612) do
+ActiveRecord::Schema.define(version: 20170422022416) do
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
   end
 
   create_table "messagetests", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
